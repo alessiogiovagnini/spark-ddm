d22ce098fdd2eecc4c87d466d64a9301f4b28b36
alessiogiovagnini <alessio.giovagnini@usi.ch>
2023-12-11 09:44:00 +0100

    updated example

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+import pyspark.context
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 
 if __name__ == '__main__':
 
-    sc = SparkContext("local", "count app")
-    words = sc.parallelize(
-        ["scala",
-         "java",
-         "hadoop",
-         "spark",
-         "akka",
-         "spark vs hadoop",
-         "pyspark",
-         "pyspark and spark"]
-    )
-    counts = words.count()
-    print("Number of elements in RDD -> %i" % counts)
+    # sc: pyspark.context.SparkContext = SparkContext("local", "count app")
+    # words = sc.parallelize(
+    #     ["scala",
+    #      "java",
+    #      "hadoop",
+    #      "spark",
+    #      "akka",
+    #      "spark vs hadoop",
+    #      "pyspark",
+    #      "pyspark and spark"]
+    # )
+    # counts = words.count()
+    # print("Number of elements in RDD -> %i" % counts)
 
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("MyFirstSparkApplication") \
+        .getOrCreate()
+
+    path: str = "./books_data.csv"
+    df = spark.read.option("header", True).csv(path)
+
+    df.show()
